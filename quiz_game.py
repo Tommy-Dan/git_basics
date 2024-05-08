97e9ebe93dba7d7cf9558cb854a2e4bd70430f5f
Tommy <ttuckerwra@gmail.com>
2024-05-08 15:42:06 +0000

    score and percentage features added

--- quiz_game.py
+++ quiz_game.py
@@ -4,38 +4,41 @@ playing = input("Do you want to play? ").lower()
 if playing != "yes":
     quit()
 print("Okay Let's play :)")
+score = 0
 
 answer = input("What does the CPU stands for? ")
 if answer == "Central Processing Unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
-    print("Please guess again: ")
 
 answer = input("What does the GPU stands for? ")
 if answer == "Graphics Processing Unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
-    print("Please guess again: ")
 
 answer = input("What does the RAM stands for? ")
 if answer == "Random Access Memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
-    print("Please guess again: ")
 
 answer = input("What does the PSU stands for? ")
 if answer == "Power Supply Unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
-    print("Please guess again: ")
 
 answer = int(input("How many megabytes make a gigabyte? "))
 if answer == 1000:
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
-    print("Please guess again: ")
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 5) * 100) + "%")
